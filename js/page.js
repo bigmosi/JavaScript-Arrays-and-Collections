a83f4d983c4cbf04b2e47f1fd679d88d4d6742e1
kinyeramos2021 <kinyeramo@gmail.com>
2021-09-20 14:21:57 +0300

    spread operator

--- js/page.js
+++ js/page.js
@@ -2,13 +2,28 @@
 
 let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
+let yearlyLabel = document.getElementById('yearlyTotal');
 
 
-let monthlySales = Array.of(12,9,3);
+let monthlySales = Array.of(1200,9000,3000);
 let monthlyLabel = Array.of('Oct', 'Nov', 'Dec');
 
 let deptLabels = Array.of('Hiking', 'Running', 'Hunting');
 let deptSales = Array.of(12,9,3);
+
+function addYearlyTotal(a,b,c) {
+  return a+b+c;
+}
+
+let octNums = Array.of(1200,1000,9000);
+let novNums = Array.of(1100,2000,9000);
+let decNums = Array.of(4000,1000,5000);
+
+let total = Array.of(addYearlyTotal(...octNums), addYearlyTotal(...novNums), addYearlyTotal(...decNums));
+alert(addYearlyTotal(...total));
+
+let yearlyTotal = addYearlyTotal(...monthlySales);
+yearlyLabel.innerHTML = "$" + yearlyTotal;
 
 // Bar
 
